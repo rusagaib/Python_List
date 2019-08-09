299873e579c509f691d52f125ecc98f784e6a205
rusagaib <33116863+rusagaib@users.noreply.github.com>
2019-08-10 02:55:07 +0700

    Create konversi_hari.py

---
+++ konversi_hari.py
@@ -0,0 +1,9 @@
+#! /usr/bin/latian/conversi_hari.py
+
+hari = input('masukkan hari: ')
+print('----------------------------------')
+t = hari / 365          #t = tahun
+sh = hari % 365         #sh = sebagai nilai sementara untuk menampung sisa hari dalam setahunya...
+b = sh / 30             #b = bulan (contoh input=480 >> 1thn, (480 % 365)/30= 115 / 30= 3 bulan )
+h = sh % 30             #h = hari (contoh input=480 >> 1thn, 3bln, (480 % 365)%30= 115 % 30= 25 hari)
+print(t, 'tahun', b,'bulan', h,'hari')       #contoh input=480 >> 1thn, 3bln, 25hari..
